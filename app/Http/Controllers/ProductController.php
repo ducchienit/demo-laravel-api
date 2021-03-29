519f8684e118f8ccebfe0c2e729093e5ce3e4f4e
tan  chien <tanchien8888@gmail.com>
2021-03-29 13:48:05 +0700

    first commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Product;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json(Product::get()); 
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $newProduct = Product::create($request->all());
+        return response()->json($newProduct);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $product = Product::find($id);
+        if(is_null($product)){
+            return response()->json('Product not found!');
+        }
+        return response()->json($product); 
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $updateProduct = Product::find($id);
+        if(is_null($updateProduct)){
+            return response()->json('Product not found!');
+        }
+        $updateProduct->update($request->all());
+        return response()->json($updateProduct); 
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $deleteProduct = Product::find($id);
+        if(is_null($deleteProduct)){
+            return response()->json('Product not found!');
+        }
+        $deleteProduct->delete();
+        return response()->json(Product::get()); 
+    }
+}
